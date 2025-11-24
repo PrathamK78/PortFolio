6abe4a7421591b2070282ee4701cc6b4ae90b21e
Pratham Kapoor <prathamkapoor1906@gmail.com>
2025-11-24 13:46:57 +0530

    Change UI

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,5 +1,28 @@
-<<<<<<< HEAD
 /*=============== EMAIL JS ===============*/
-=======
-/*=============== EMAIL JS ===============*/
->>>>>>> e98b1d08245ef4dd27f15574de0593ac2935a8a8
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message') 
+
+const sendEmail = (e) => {
+  e.preventDefault();
+  
+  //serviceID - templateID - #form - publicKey
+  emailjs.sendForm('service_hpjt2at', 'template_1xhnamd', '#contact-form', 'yeOe4Jgk6aDgqZbgN')
+    .then(() => {
+      //show sent message
+      contactMessage.textContent = 'Message sent successfully ✅'
+      
+      //remove message after 5 secs
+      setTimeout(() => {
+        contactMessage.textContent = ''
+      }, 5000)
+      
+      //Clear input fields
+      contactForm.reset()
+  
+    },()=>{
+      //show error message
+      contactMessage.textContent = 'Message not sent (Error in service)❌'
+    })
+}
+
+contactForm.addEventListener('submit',sendEmail)
